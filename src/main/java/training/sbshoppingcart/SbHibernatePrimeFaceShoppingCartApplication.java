0455723464f655cf3303df38b65e4e912117acd2
dodinhtiep <204.angel.demon@gmail.com>
2018-12-27 18:10:20 +0700

    init project

---
+++ src/main/java/training/sbshoppingcart/SbHibernatePrimeFaceShoppingCartApplication.java
@@ -0,0 +1,14 @@
+package training.sbshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHibernatePrimeFaceShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHibernatePrimeFaceShoppingCartApplication.class, args);
+	}
+
+}
+
